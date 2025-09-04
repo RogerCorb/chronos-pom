c1c9a3535bf91740ba611fbc5fae83e0f4f97c1c
RogerCorb <rogeriocgprest@gmail.com>
2025-09-04 17:22:03 -0300

    css module.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import './styles/theme.css';
 import './styles/global.css';
+import { Heading } from './components/Heading';
 
 export function App() {
 
   console.log('oi');
 
   return (
-    <> 
-      <h1>olá mundo</h1>
+    <>
+      <Heading/>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
