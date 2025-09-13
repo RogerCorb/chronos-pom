5e4c08e0274387a34ec6465293f27bcb2a69b9bb
RogerCorb <rogeriocgprest@gmail.com>
2025-09-13 20:23:59 -0300

    create genericHtml

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,7 @@ import { Home } from './pages/Home';
 import './styles/theme.css';
 import './styles/global.css';
 
-export function App() {
 
-  return (  
-      <Home/>  
-  )
+export function App() {
+  return <Home/>    
 }
