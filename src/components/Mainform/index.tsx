d21e638617aebe65ee5d5c6166f29dbdd980226b
RogerCorb <rogeriocgprest@gmail.com>
2025-09-16 20:45:47 -0300

    refactor Mainform

--- src/components/Mainform/index.tsx
+++ src/components/Mainform/index.tsx
@@ -2,15 +2,31 @@ import { PlayCircleIcon } from "lucide-react";
 import { Cycles } from "../Cycles";
 import { DefaultInput } from "../DefaultInput";
 import { DefaultButton } from "../DefaultButton";
+import { useState } from "react";
 
 
 export function MainForm() {
 
+    const [taskName,setTaskName] = useState('');
+
+    function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) { 
+        console.log(taskName);
+        event.preventDefault();
+        
+    }
+
     return (
 
-        <form className='form' action=''>
+        <form onSubmit={handleCreateNewTask} className='form' action=''>
             <div className='formRow'>
-                <DefaultInput labelText='task' id='meuInput' type='text' placeholder='Digite algo' />
+                <DefaultInput 
+                  labelText='task'
+                  id='meuInput'
+                  type='text'
+                  placeholder='Digite algo'
+                  value={taskName} // 
+                  onChange={e=>setTaskName(e.target.value)}//input controlado cada letra digitada é salva em tempo real
+                />
             </div>
             <div className='formRow'>
                 <p>Lorem ipsum dolor sit amet.</p>
