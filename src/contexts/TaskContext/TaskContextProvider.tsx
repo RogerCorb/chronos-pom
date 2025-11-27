e692d3d5a740d4f1fd802aa78ed4e9eff8ee4d8e
RogerCorb <rogeriocgprest@gmail.com>
2025-11-26 21:28:03 -0300

    create timeworker.js

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -2,6 +2,7 @@ import { useEffect, useReducer } from "react";
 import { initialTaskState } from "./initialTaskState";
 import { TaskContext } from "./TaskContext";
 import { taskReducer } from "./taskReducer";
+import { TimerWorkerManager } from "../../workers/TimerWorkerManager";
 
 type TaskContextProviderProps = {
   children: React.ReactNode;
@@ -9,9 +10,25 @@ type TaskContextProviderProps = {
 
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
   const [state, dispatch] = useReducer(taskReducer, initialTaskState);
+
+  const worker = TimerWorkerManager.getInstance();
+
+  worker.onmessage((e) => {
+    const countDownSeconds = e.data;
+    console.log(countDownSeconds);
+    if (countDownSeconds <= 0) {
+      console.log("worker COMPLETED ");
+      worker.terminate();
+    }
+  });
+
   useEffect(() => {
-    console.log(state);
-  }, [state]); // monitorando o state com o hook use Effect() quando for inicializada a tarefa
+    if (!state.activeTask) {
+      console.log("worker terminado ");
+      worker.terminate();
+    }
+    worker.postMessage(state);
+  }, [worker, state]); // monitorando o state com o hook use Effect() quando for inicializada a tarefa
 
   return (
     <TaskContext.Provider value={{ state, dispatch }}>
---
+++ src/workers/timerWorker.js
@@ -0,0 +1,21 @@
+let isRunning = false;
+
+self.onmessage = function (event) {
+  if (isRunning) return;
+  isRunning = true;
+
+  const state = event.data;
+  const { activeTask, secondsRemaining } = state;
+
+  const endDate = activeTask.startDate + secondsRemaining * 1000;
+  const now = Date.now();
+  let countDownSeconds = Math.ceil((endDate - now) / 1000);
+
+  function tick() {
+    const now = Date.now();
+    countDownSeconds = Math.floor((endDate - now) / 1000); // 1000 miliseconds que então vai dar em segundos 1,2,3,4,5...
+    self.postMessage(countDownSeconds);
+  }
+
+  tick();
+};
